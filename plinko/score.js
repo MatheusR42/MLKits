dd25b564981b21b9ee3e9aaa02259a8a21ec0bf1
MatheusR42 <matheus.araujo.r42@gmail.com>
2019-03-30 16:48:52 -0300

    13. Finishing KNN Implementation

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,44 @@
+const outputs = [];
+const predictedPoint = 0;
+const k = 100;
+const COL_POSITION = 0;
+const COL_BUCKET = 3;
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const result = _.chain(outputs)
+    //get bucket and distance between predicted point
+    .map(row => [row[COL_BUCKET], distance(row[COL_POSITION], predictedPoint)])
+
+    //sort by distance
+    .sortBy(row => row[1])
+
+    //get nearest values
+    .slice(0, k)
+
+    //count how many times each bucket appears
+    .countBy(row => row[0])
+    
+    //get the bucket that repeat most
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+
+  alert(`Droping from position ${predictedPoint} your ball probability will fall in ${result} bucket`);
 }
+
+function distance(point, point2) {
+  return Math.abs(point - point2)
+}
+
+
+
+
 
