e5a7c7ce1d33c1eac9c9652078e1130e4aecf669
MatheusR42 <matheus.araujo.r42@gmail.com>
2019-03-30 23:43:59 -0300

    19. Gauging Accuracy

--- plinko/score.js
+++ plinko/score.js
@@ -1,5 +1,4 @@
 const outputs = [];
-const predictedPoint = 0;
 const k = 100;
 const COL_POSITION = 0;
 const COL_BUCKET = 3;
@@ -10,7 +9,19 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const result = _.chain(outputs)
+  
+  const [testSet, trainingSet] = splitDataSet(outputs, 10);
+  
+  for (let i = 0; i < testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][COL_POSITION]);
+    console.log(bucket, testSet[i][COL_BUCKET]);
+  }
+
+  // alert(`Droping from position ${predictedPoint} your ball probability will fall in ${result} bucket`);
+}
+
+function knn(data, predictedPoint) {
+  return _.chain(data)
     //get bucket and distance between predicted point
     .map(row => [row[COL_BUCKET], distance(row[COL_POSITION], predictedPoint)])
 
@@ -30,12 +41,10 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value();
-
-  alert(`Droping from position ${predictedPoint} your ball probability will fall in ${result} bucket`);
 }
 
-function distance(point, point2) {
-  return Math.abs(point - point2)
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB)
 }
 
 
